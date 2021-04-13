1738a8dbb549f66b201f0bfb69e022d608bf166b
CarolineCunha <caroline.cunha.b@hotmail.com>
2021-04-13 07:32:49 -0300

    configurando fontes

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -16,6 +16,12 @@ export const theme = extendTheme({
 
         }
     },
+
+    fonts: {
+        heading: 'Roboto',
+        body: 'Roboto',
+    },
+
     styles: {
         global: {
             body: {
