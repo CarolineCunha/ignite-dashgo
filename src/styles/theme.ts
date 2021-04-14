24391eb35d3e640c7325def353b59bfab64f1a86
CarolineCunha <caroline.cunha.b@hotmail.com>
2021-04-14 06:59:57 -0300

    pagina sign in

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -18,8 +18,8 @@ export const theme = extendTheme({
     },
 
     fonts: {
-        heading: 'Roboto',
-        body: 'Roboto',
+        heading:'Roboto',
+        body:'Roboto',
     },
 
     styles: {
