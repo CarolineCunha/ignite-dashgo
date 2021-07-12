078b3997ec5e8f41c3423bf748aaea72eb3cb62b
CarolineCunha <caroline.cunha.b@hotmail.com>
2021-07-12 18:49:55 -0300

    header responsivo

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,11 +1,17 @@
-import { Flex, Text, Input, Icon, HStack, Box, Avatar } from '@chakra-ui/react'
-export function Profile() {
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from '@chakra-ui/react';
+
+interface ProfileProps {
+    showProfileData ?: boolean;
+}
+export function Profile({ showProfileData = true}: ProfileProps) {
     return(
         <Flex align="center">
-                    <Box mr="4" textAlign="right">
+                    {showProfileData && (
+                        <Box mr="4" textAlign="right">
                         <Text>Caroline Cunha</Text>
                         <Text color="gray.300" fontSize="small">caroline.cunha.b@hotmail.com</Text>
                     </Box>
+                    )}
                     <Avatar size="md" name="Caroline Cunha" src="https://github.com/CarolineCunha.png" />
                 </Flex>
     );
